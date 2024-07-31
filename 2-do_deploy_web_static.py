3c099c3240084bd939d289a6cc5705c5d7125a27
PreciousEzeigbo <preciousezeigbo@gmail.com>
2024-07-31 13:49:33 -0700

    Fixing error

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['54.89.109.87', '100.25.190.21']
+env.hosts = ['100.25.33.107', '54.196.29.103'
 
 
 def do_deploy(archive_path):
